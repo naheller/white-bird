a169e16bb3c287bc16ae00cc631e5831b2829aa1
Nathan Heller <naheller@gmail.com>
2019-11-13 21:07:03 -0800

    Org page initial

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className="h-full flex items-center justify-center">
-      <h1 className="font-semibold text-xl text-gray-900 tracking-tight">
-        <Link to="/search">White Bird Help Book</Link>
-      </h1>
+      <Link to="/search" className="text-lg font-semibold text-gray-800">
+        White Bird Help Book
+      </Link>
     </nav>
   )
 }
