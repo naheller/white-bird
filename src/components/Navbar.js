4fab06dc3a0e88b0b37f0a7b287bd405c2409bed
Nathan Heller <naheller@gmail.com>
2019-11-17 17:01:50 -0800

    Remove link from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
 
 const Navbar = () => {
   return (
     <nav className="h-full flex items-center justify-center text-gray-800">
       <svg
-          className="fill-current w-4 h-4 mr-2"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
-          <path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"/>
-        </svg>
-      <Link to="/search" className="text-lg font-semibold">
-        White Bird Help Book
-      </Link>
+        className="fill-current w-4 h-4 mr-2"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z" />
+      </svg>
+      <h1 className="text-lg font-semibold">White Bird Help Book</h1>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
